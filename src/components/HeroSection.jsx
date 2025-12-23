5bcc3434af4b1aab4feb8d0d3563301f1d00807b
Abdel-Rahman Youssef <abodyoussef2015@gmail.com>
2025-12-23 15:29:09 +0300

    set all the buttons links

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,8 @@
+import { useParams, useNavigate } from "react-router-dom";
+
 function HeroSection() {
+    const navigate = useNavigate()
+    const {lang} = useParams();
     return (
         <section className="relative min-h-screen min-h-[600px] flex items-center justify-center overflow-hidden" id="home">
             <div className="absolute inset-0">
@@ -18,10 +22,10 @@ function HeroSection() {
                         طريقك إلى الإحتراف المهني
                     </p>
                     <div className="flex gap-5 justify-center animate-fadeInUp animation-delay-400">
-                        <button href="/" className="mx-2 py-[12px] px-[12px] rounded-full! cursor-pointer hover:text-[#1a2555] text-white lg:text-[16px] sm:text-[14px] font-bold bg-gradient-to-r! from-[#23A0D0]! to-68% to-[#3CBEB3]! focus:outline-none! shadow-md! transition! hover:opacity-80! !leading-[1.25] duration-300">
+                        <a href="/files/profile.pdf" download="أكاديمية_المهارات_التطبيقية_الملف_التعريفي.pdf" className="mx-2 py-[12px] px-[12px] rounded-full! cursor-pointer hover:text-[#1a2555] text-white lg:text-[16px] sm:text-[14px] font-bold bg-gradient-to-r! from-[#23A0D0]! to-68% to-[#3CBEB3]! focus:outline-none! shadow-md! transition! hover:opacity-80! !leading-[1.25] duration-300 inline-block">
                             <i className="fas fa-download"></i> الملف التعريفي
-                        </button>
-                        <button href="/" className="mx-2 py-[12px] px-[12px] rounded-full! cursor-pointer hover:text-[#1a2555] text-black bg-white lg:text-[16px] sm:text-[14px] font-bold focus:outline-none! shadow-md! transition! hover:bg-gradient-to-r! hover:from-[#23A0D0]! hover:to-68% hover:to-[#3CBEB3] hover:text-white! !leading-[1.25] duration-300">التسجيل</button>
+                        </a>
+                        <button onClick={() => navigate(`/${lang}/registration`)} className="mx-2 py-[12px] px-[12px] rounded-full! cursor-pointer hover:text-[#1a2555] text-black bg-white lg:text-[16px] sm:text-[14px] font-bold focus:outline-none! shadow-md! transition! hover:bg-gradient-to-r! hover:from-[#23A0D0]! hover:to-68% hover:to-[#3CBEB3] hover:text-white! !leading-[1.25] duration-300">التسجيل</button>
                     </div>
                 </div>
             </div>
